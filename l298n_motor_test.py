ebe923003b3ae764d921f5470396176f683335f9
jhawk7 <jhawk154@gmail.com>
2020-04-05 18:19:22 -0400

    first commit

---
+++ l298n_motor_test.py
@@ -0,0 +1,12 @@
+from l298n import l298n
+import RPi.GPIO as GPIO
+
+def motorTest(pin1, pin2):
+	motor = l298n(pin1, pin2)
+	motor.forwards()
+	motor.backwards()
+	motor.stop()
+
+# using GPIO pin numbers not board pin numbers
+motorTest(17, 27)
+
---
+++ sonar_test.py
@@ -0,0 +1,15 @@
+from adafruit_circuitpython_hcsr04 import HCSR04
+import RPi.GPIO as GPIO
+
+def sonarTest(trigger, echo):
+	sonar = HCSR04(trigger_pin=trigger, echo_pin=echo)
+
+	while True:
+		try:
+			print((sonar.distance))
+		except RuntimeError:
+			print("Retrying!")
+		time.sleep(0.1)
+
+# using GPIO pin numbers opposed to board pin numbers
+sonarTest(3, 4)
