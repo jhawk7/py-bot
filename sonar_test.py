c307fac2a749bdc98fd42e314bbdcb7eafa07bd3
jhawk7 <jhawk154@gmail.com>
2020-04-07 23:41:44 -0400

    fixing sonar test

--- sonar_test.py
+++ sonar_test.py
@@ -1,15 +1,26 @@
 from adafruit_circuitpython_hcsr04 import HCSR04
 import time
+import RPi.GPIO as GPIO
+
+MIN_DISTANCE = 35
 
 def sonarTest(trigger, echo):
+	# GPIO_18 => pin 12
+	LED = 18
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(LED,GPIO.OUT)
 	sonar = HCSR04(trigger_pin=trigger, echo_pin=echo)
 
 	while True:
 		try:
-			print((sonar.distance))
+			if sonar.distance > MIN_DISTANCE:
+				GPIO.output(LED, GPIO.HIGH)
+				print((sonar.distance))
 		except RuntimeError:
 			print("Retrying!")
 		time.sleep(0.1)
 
 # using GPIO pin numbers opposed to board pin numbers
 sonarTest(3, 4)
+
+
