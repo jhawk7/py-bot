2eaac74da4ce7b7f7be680839039cd808958389b
jhawk7 <jhawk154@gmail.com>
2020-04-11 16:25:07 -0400

    fixing beep

--- py-bot.py
+++ py-bot.py
@@ -39,15 +39,18 @@ motor = L298N(IN1, IN2, IN3, IN4, ENA, ENB)
 
 def beep():
 	p = GPIO.PWM(speaker, 50)    # create an object p for PWM on port 25 at 50 Hertz  
-	p.start(70)             	 # start the PWM on 70 percent duty cycle  
 
 	while not STOP:
+		p.start(70)             	 # start the PWM on 70 percent duty cycle  
 		#for x in range(200, 2200):
  		#p.ChangeFrequency(x)     	  # change the frequency to x Hz 
  		#time.sleep(0.0001)
  		p.ChangeFrequency(30)
+ 		time.sleep(0.001)
  		p.ChangeFrequency(70)
+ 		time.sleep(0.001)
  		p.ChangeFrequency(50)
+ 		time.sleep(0.001)
  		p.stop()
  		time.sleep(10)
 		#p.stop()                     # stop the PWM output  
