a8b82c680ea0ac4653828ca4a6f78503d37cefb6
jhawk7 <james.hawkins@careerbuilder.com>
2022-04-28 14:47:01 -0400

    adding sonic servo test

---
+++ sonic_servo_test.py
@@ -0,0 +1,21 @@
+from UltrasonicServo import UltrasonicServo
+import time
+import RPi.GPIO as GPIO
+
+# Sonar GPIO Pins
+TRIG = 22
+ECHO = 23
+SERVO = 14 #sonar servo pin
+MIN_DISTANCE = 40 #in cm
+
+def lookTest():
+    sonar_servo = UltrasonicServo(TRIG, ECHO, MIN_DISTANCE, SERVO)
+    left = sonar_servo.checkLeft()
+    print("Object detected on left") if left else print("No object on left")
+    time.sleep(1)
+    right = sonar_servo.checkRight()
+    print("Object detected on right") if right else print("No object on right")
+    return
+
+lookTest()
+GPIO.cleanup()
