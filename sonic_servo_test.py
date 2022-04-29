63bcf458233129dfe2386eecfccf727d4102a14e
jhawk7 <james.hawkins@careerbuilder.com>
2022-04-28 21:50:35 -0400

    fixing output statments

--- sonic_servo_test.py
+++ sonic_servo_test.py
@@ -11,13 +11,13 @@ MIN_DISTANCE = 40 #in cm
 def lookTest():
     sonar_servo = UltrasonicServo(TRIG, ECHO, MIN_DISTANCE, SERVO)
     left = sonar_servo.checkLeft()
-    print("Object detected on left") if left else print("No object on left")
+    print("Object detected in range on left") if left else print("No object in range on left")
     time.sleep(1)
     right = sonar_servo.checkRight()
-    print("Object detected on right") if right else print("No object on right")
+    print("Object detected in range on right") if right else print("No object in range on right")
     time.sleep(1)
     center = sonar_servo.checkFront()
-    print("Object detected on center") if center else print("No object in front")
+    print("Object detected in range in front") if center else print("No object in range in front")
     return
 #
 
